2b418b4fd7b2d48accd6e654f2fe0b1a62428015
elisebrenna <elisebrenna@gmail.com>
2025-06-17 20:43:37 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,38 +1,68 @@
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import "./App.css";
 import { Hjem } from "./components/Hjem";
-import { Header } from "./components/Header";
 import { Menu } from "./components/Menu";
 import { Kontakt } from "./components/Kontakt";
-import { Om } from "./components/Om";
 import { Områder } from "./components/Områder";
-
-import { data } from "./data/data.js";
+import { Om } from "./components/Om";
+import { PropertyDetails } from "./components/PropertyDetails";
+import SunriseLogo from "./images/logo.png";
+import { NavLink } from "@fremtind/jkl-core";
+import { PropertyProvider } from "./components/PropertyContext";
+import { useNavigate } from "react-router-dom";
 
 const App = () => {
-  const objects = data.property;
-  console.log(data)
+  const pages = [
+    { title: "Hjem", path: "/" },
+    { title: "Om", path: "/om" },
+    { title: "Boligobjekter", path: "/objekter" },
+    { title: "Kontakt oss", path: "/kontakt" },
+  ];
 
   return (
-    <div className="jkl">
-      <div className="header-wrapper">
-        <Header />
-        <Menu />
-      </div>
+    <BrowserRouter>
+      <main>
+        {/* <PropertyProvider> */}
+        <header>
+          <div className="logo-container">
+            <img src={SunriseLogo} alt="Sunrisehomes logo" className="logo" />
+          </div>
+          <nav>{/* <Menu /> */}</nav>
+        </header>
+        <div> Siden er under konstruksjon.</div>
 
+        {/* <Routes>
+            <Route path="/" element={<Hjem />} />
+            <Route path="/om" element={<Om />} />
+            <Route path="/objekter" element={<Områder />} />
+            <Route path="/kontakt" element={<Kontakt />} />
+            <Route path="/property/:id" element={<PropertyDetails />} />
+          </Routes>
 
-
-      <BrowserRouter>
-      <Routes>
-      <Route path="/" element={<Hjem />} />
-        <Route path="/om" element={<Om />} />
-        <Route path="/objekter" element={<Områder objects={objects} />} />
-        <Route path="/kontakt" element={<Kontakt />} />
-      </Routes>
-
-      </BrowserRouter>
-      {/* <Footer /> */}
-    </div>
+          <footer>
+            <div className="footer-element-content">
+              Sunrise Homes <br />
+              Haugerudbråtan 17
+              <br />
+              3408 Tranby <br />
+              Org.nr: 929 380 797 <br />
+              E-post: post@sunrisehomes.no
+            </div>
+            <div className="footer-element-content">
+              {pages.map((page) => (
+                <NavLink
+                  key={page.path}
+                  href={page.path}
+                  className="menu-link-footer"
+                >
+                  {page.title}
+                </NavLink>
+              ))}
+            </div>
+          </footer>
+        </PropertyProvider> */}
+      </main>
+    </BrowserRouter>
   );
 };
 
